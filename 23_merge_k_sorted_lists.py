24b7b5c30298a1c2d09d3c1bee850fbf11b03dde
Pamela Tabak <pamelatabak@poli.ufrj.br>
2021-05-24 17:03:29 -0300

    updating problem 23

--- 23_merge_k_sorted_lists.py
+++ 23_merge_k_sorted_lists.py
@@ -3,19 +3,22 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
+
+
 class Solution:
-    def checkIfFinished (self, lists):
+    def checkIfFinished(self, lists):
         for lst in lists:
             if (lst != None):
                 return False
         return True
-    
+
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
         if (len(lists) == 0):
             return None
-        
-        result = []
+
+        head: ListNode = ListNode(0)
+        result = head
+
         last_number = -1000000
         while (not self.checkIfFinished(lists)):
             smallestNumber = 1000000
@@ -23,7 +26,6 @@ class Solution:
             for i in range(0, len(lists)):
                 node = lists[i]
                 if (node == None):
-                    #print(lists)
                     continue
                 value = node.val
                 if (value == last_number):
@@ -34,14 +36,9 @@ class Solution:
                     smallestNumber = value
                     index = i
             last_number = smallestNumber
-            result.append(smallestNumber)
+            result.next = ListNode(smallestNumber)
+            result = result.next
             lists[index] = lists[index].next
             if (lists[index] == None):
                 del lists[index]
-        if (len(result) == 0):
-            return None
-        #we now need to convert the list to a linked-list
-        linkedList: ListNode = ListNode(result[-1])
-        for i in range(len(result) - 2, -1, -1):
-            linkedList: ListNode = ListNode(result[i], linkedList)
-        return linkedList
+        return head.next
